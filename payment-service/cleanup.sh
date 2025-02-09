eb656501742882a33f6db21f71ba1a290a8f7475
goravecz <oravecz.gabor@protonmail.com>
2025-02-09 18:32:43 +0100

    feat(PP-8): integrate transaction db

--- payment-service/cleanup.sh
+++ payment-service/cleanup.sh
@@ -6,8 +6,8 @@ docker ps -q | xargs -r docker stop
 echo "Removing matching containers..."
 docker ps -a --format '{{.ID}} {{.Names}}' | awk '/payment|pga|postgre|transaction/ {print $1}' | xargs -r docker rm -f
 
-echo "Removing matching images..."
-docker images --format '{{.ID}} {{.Repository}}' | awk '/payment|pga|postgre/ {print $1}' | xargs -r docker rmi -f
+#echo "Removing matching images..."
+#docker images --format '{{.ID}} {{.Repository}}' | awk '/payment|pga|postgre/ {print $1}' | xargs -r docker rmi -f
 
 echo "Removing matching volumes..."
 docker volume ls --format '{{.Name}}' | awk '/payment|pga|postgre/ {print $1}' | xargs -r docker volume rm
--- payment-service/src/main/java/com/bank/paymentservice/repository/TransactionsRepository.java
+++ payment-service/src/main/java/com/bank/paymentservice/repository/TransactionsRepository.java
@@ -1,4 +1,11 @@
 package com.bank.paymentservice.repository;
 
-public class TransactionsRepository {
+import com.bank.paymentservice.model.Transaction;
+import java.util.UUID;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TransactionsRepository extends JpaRepository<Transaction, Long> {
+  boolean existsById(UUID id);
 }
