e1ae038215f532bc173ba05bc22850522eeb540a
goravecz <oravecz.gabor@protonmail.com>
2025-02-10 17:02:08 +0100

    feat(PP-14): add consumer skeleton

---
+++ notification-processor/src/test/java/com/bank/notificationprocessor/NotificationProcessorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bank.notificationprocessor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotificationProcessorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
