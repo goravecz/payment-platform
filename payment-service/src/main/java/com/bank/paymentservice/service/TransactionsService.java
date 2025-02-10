114c7e564c4c8be76f148bf16f70d304021ca1dc
goravecz <oravecz.gabor@protonmail.com>
2025-02-10 02:49:16 +0100

    feat(PP-12): fix transaction management for transactions

--- payment-service/src/main/java/com/bank/paymentservice/service/TransactionsService.java
+++ payment-service/src/main/java/com/bank/paymentservice/service/TransactionsService.java
@@ -3,8 +3,8 @@ package com.bank.paymentservice.service;
 import com.bank.paymentservice.error.DuplicateTransactionException;
 import com.bank.paymentservice.model.Transaction;
 import com.bank.paymentservice.repository.TransactionsRepository;
+import org.springframework.transaction.annotation.Isolation;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.transaction.annotation.Propagation;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -16,10 +16,10 @@ public class TransactionsService {
       this.transactionsRepository = transactionsRepository;
   }
 
-@Transactional(propagation = Propagation.REQUIRED)
+  @Transactional(isolation = Isolation.SERIALIZABLE)
   public Transaction save(Transaction transaction) {
-    // The unique constraint on the (primary key) id should throw an exception if the transaction
-    // already exists, but it doesn't. So I have to check it manually.
+    // The unique constraint on the (primary key) id should throw an DataIntegrityViolationException
+    // if the transaction already exists, but it doesn't. So I have to check it manually.
     if (transactionsRepository.existsById(transaction.getId())) {
       throw new DuplicateTransactionException(transaction.getId());
     }
